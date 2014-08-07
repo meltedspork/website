b8a0dd172556942e04f181da9e1ba546cb61844f
Jason Salerno <mrjsin@gmail.com>
2014-08-06 22:43:27 -0500

    cleaning up files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,12 +27,7 @@ require.config({
         ,underscore: '../bower_components/lodash/dist/lodash'
         ,bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap'
         ,createjs: 'libs/createjs-2013.12.12.min'
-        //,easeljs: '../bower_components/easeljs/lib/easeljs-0.7.1.min'
-        ,movieclip: '../bower_components/easeljs/lib/movieclip-0.7.1.min'
-        //,tweenjs: '../bower_components/createjs-tweenjs/lib/tween.min'
-        //,preloadjs: '../bower_components/createjs-preloadjs/lib/preloadjs-0.4.1.min'
-        //,createjs: '//code.createjs.com/createjs-2013.12.12.min'
-        //,movieclip: '//code.createjs.com/movieclip-0.7.1.min'
+        ,movieclip: 'libs/movieclip-0.7.1.min'
         ,weathercanvas: 'canvas/weathercanvas'
     }
 });
