35521f7e41176c610bf1764935f472f7bf285ab0
Jason Salerno <mrjsin@gmail.com>
2014-08-06 22:24:09 -0500

    console.log

--- app/server.js
+++ app/server.js
@@ -3,5 +3,7 @@ var serveStatic = require('serve-static');
 
 var app = connect();
 
+console.log("__dirname",__dirname);
+
 app.use(serveStatic(__dirname, {'index': ['index.html']}));
 app.listen(9000);
