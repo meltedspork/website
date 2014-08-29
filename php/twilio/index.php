8465c20fc85fec9d85216bdce5582193bccb53c7
Jason Salerno <mrjsin@gmail.com>
2014-08-29 13:58:05 -0500

    Testing something.

--- php/twilio/index.php
+++ php/twilio/index.php
@@ -2,9 +2,10 @@
 require '../../vendor/autoload.php';
 
 $app = new \Slim\Slim();
-
+/*
 $app->get('/sms/', function () {
-    require 'sms.php';
-});
+   // require 'sms.php';
+    echo echo $path_parts['dirname'];
+});*/
 
 $app->run();
