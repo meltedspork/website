11a9fc6941869311eb6469f3ec438efa12a62b2f
Jason Salerno <mrjsin@gmail.com>
2014-08-30 20:27:26 -0500

    testing slim

--- php/index.php
+++ php/index.php
@@ -3,10 +3,12 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+/*
 $app->get('/:name', function ($name) {
    echo "Hello $name";
    require __DIR__.'/' . $name . '.php';
 });
+*/
 
 $app->get('/:filepath/:functionName', function ($filepath,$functionName) use ($app) {
 	require __DIR__.'/' . $filepath . '.php';
