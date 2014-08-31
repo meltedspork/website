67df176eb5b367962a6ea16662795d27a8d8bbef
Jason Salerno <mrjsin@gmail.com>
2014-08-31 00:53:33 -0500

    Finally fixed content-type issue.

--- php/index.php
+++ php/index.php
@@ -3,16 +3,9 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
-/*
-$app->get('/:name', function ($name) {
-   echo "Hello $name";
-   require __DIR__.'/' . $name . '.php';
-});
-*/
 
 $app->get('/:filepath/:functionName', function ($filepath,$functionName) use ($app) {
 	require __DIR__.'/' . $filepath . '.php';
-	//echo "functionName: ".$functionName;
 });
 
 $app->run();
