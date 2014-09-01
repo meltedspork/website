5a6a9c174cbf166997659dd2c316901fd2c1de2a
Jason Salerno <mrjsin@gmail.com>
2014-09-01 15:14:16 -0500

    Updated php code to be more like Restful API

--- php/twilio.php
+++ php/twilio.php
@@ -5,7 +5,9 @@ $app = new \Slim\Slim();
 
 function sms($app) {
     // start the session
-    session_start();
+    if(!isset($_SESSION)) {
+        session_start();
+    }
 
     // get the session varible if it exists
     if (isset($_SESSION['counter'])) {
@@ -13,6 +15,32 @@ function sms($app) {
     } else {
         $counter = 0;
     }
+
+    $funcObj = array("Num");
+    $funcStr = false;
+
+    $body = $app->request()->params('Body');
+
+    if ($body != '') {
+        $temp_body = explode(",", $body)[0];
+        $check_sms_app = ucfirst(strtolower($temp_body));
+
+        foreach($funcObj as $obj) {
+            if ($obj == $check_sms_app) {
+                $_SESSION['sms_app'] = $check_sms_app;
+                $body = str_replace($temp_body . ',', '', $body);
+                $funcStr = true;
+                break;
+            }
+        }
+    } else {
+        $_SESSION['sms_app'] = 'Init';
+    }
+
+    if (!isset($_SESSION['sms_app']) || empty($_SESSION['sms_app']) || $_SESSION['sms_app'] == 'Init') {
+         $_SESSION['sms_app'] = 'Init';
+    }
+    $sms_app = $_SESSION['sms_app'];
 
     $timeout = 300; // Number of seconds until it times out.
 
@@ -26,56 +54,52 @@ function sms($app) {
             session_destroy();
             session_start();
             $counter = 0;
+            if(!$funcStr) {
+                $sms_app = 'Init';
+            }
         }
     }
 
     // Update the timout field with the current time.
     $_SESSION['timeout'] = time();
 
-     // increment it
+    // increment it
     $counter++;
 
     // save it
     $_SESSION['counter'] = $counter;
+    $_SESSION['sms_app'] = $sms_app;
 
-
+    //$reply = $_SESSION['timeout'].' | '.$_SESSION['counter'].' | '.$_SESSION['sms_app'].' | '.$body;
     $reply = '';
 
-    if($counter > 1) {
-        $number = $reply = $app->request()->params('Body');
-
-        if (!is_numeric($number) || $number == '' || $number == null) {
-            $number = 'random';
-        }
-        $reply = $number;
-
-       $url = 'http://numbersapi.com/'.$number.'/trivia?notfound=ceil&json';
-
-        //  Initiate curl
-        $ch = curl_init();
-        // Disable SSL verification
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // Will return the response, if false it print the response
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // Set the url
-        curl_setopt($ch, CURLOPT_URL,$url);
-        // Execute
-        $result=curl_exec($ch);
-        // Closing
-        curl_close($ch);
-
-        // Will dump a beauty json :3
-        // var_dump(json_decode($result));
-        $response = json_decode($result,true);
-        $reply = $response['text'];
-
-        if ($number == 'random') {
-            $reply = 'Random Number: '.$reply;
-        }
-
-    } else {
-        $reply = 'Trivia time! Pls, reply back w number.';
+    if(is_callable('sms' . $sms_app)) {
+        $reply = call_user_func('sms' . $sms_app,$body);
     }
+
+    return '#' . $counter. ': ' .$reply;
+}
+
+function smsInit($body) {
+    return 'Send NUM,{number} for triva number: '.$body;//, TIME for current time, TEMP for current weather';
+}
+
+function smsNum($number) {
+
+    if (!is_numeric($number) || $number == '' || $number == null) {
+        $number = 'random';
+    }
+
+    $url = 'http://numbersapi.com/'.$number.'/trivia?notfound=ceil&json';
+
+    $response = getCurl($url);
+
+    $reply = $response['text'];
+
+    if ($number == 'random') {
+        $reply = 'Random Number: '.$reply;
+    }
+
     return $reply;
 }
 
@@ -93,5 +117,25 @@ $app->get("/twilio/:functionName", function ($functionName) use ($app) {
     }
 
 });
+
+function getCurl($url) {
+    //  Initiate curl
+    $ch = curl_init();
+    // Disable SSL verification
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    // Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    // Set the url
+    curl_setopt($ch, CURLOPT_URL,$url);
+    // Execute
+    $result=curl_exec($ch);
+    // Closing
+    curl_close($ch);
+
+    // Will dump a beauty json
+    // var_dump(json_decode($result));
+
+    return json_decode($result,true);
+}
 
 $app->run();
