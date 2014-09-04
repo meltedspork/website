f97947eab06834de7fad40651957259671233afe
Jason Salerno <mrjsin@gmail.com>
2014-09-04 15:48:38 -0500

    testing server php

--- php/twilio.php
+++ php/twilio.php
@@ -23,7 +23,7 @@ function sms($app) {
     $body = $app->request()->params("Body");
 
     if ($body != "") {
-        $temp_body = explode(",", $body)[0];
+        $temp_body = $body;//explode(",", $body)[0];
         $check_sms_app = ucfirst(strtolower($temp_body));
 
         foreach($funcObj as $obj) {
