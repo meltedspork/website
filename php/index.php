7d0ae3a1f1523edcdb1ed6996ca49f400bbdcdf2
Jason Salerno <mrjsin@gmail.com>
2014-09-05 13:50:21 -0500

    creating branch for this. since its messing up master branch. it is proper process anyway!

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('/:filepath/:functionName(/:varName)', function ($filepath,$functionName,$varName) use ($app) {
+$app->get('/:filepath/:functionName/(:varName)', function ($filepath,$functionName,$varName) use ($app) {
 	echo 'varName: '.$varName;
 	//require __DIR__.'/' . $filepath . '.php';
 });
