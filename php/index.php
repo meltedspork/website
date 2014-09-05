40aebb2638c32c76e5e1f929182db72746682ec7
Jason Salerno <mrjsin@gmail.com>
2014-09-05 13:51:37 -0500

    Reverted

--- php/index.php
+++ php/index.php
@@ -4,9 +4,8 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('/:filepath/:functionName(/:varName)', function ($filepath,$functionName,$varName) use ($app) {
-	echo 'varName: '.$varName;
-	//require __DIR__.'/' . $filepath . '.php';
+$app->get('/:filepath/:functionName/', function ($filepath,$functionName) use ($app) {
+	require __DIR__.'/' . $filepath . '.php';
 });
 
 $app->run();
