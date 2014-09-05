de8c14228ba8a8bacbbf61a7596e05bd1ddb6cb5
Jason Salerno <mrjsin@gmail.com>
2014-09-05 14:17:42 -0500

    Testing optional var

--- php/index.php
+++ php/index.php
@@ -4,10 +4,11 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('/:filepath/:functionName', function ($filepath,$functionName) use ($app) {
+$app->get('/:filepath/:functionName(/(:varName))', function ($filepath,$functionName,$varName) use ($app) {
 	$home = new SimpleXMLElement("<Route/>");
     $route = $home->addChild("filepath",$filepath);
     $route = $home->addChild("functionName",$functionName);
+    $route = $home->addChild("varName",$varName);
 	echo $home->asXML();
 
 	//require __DIR__.'/' . $filepath . '.php';
