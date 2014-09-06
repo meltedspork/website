5a8e942370810aa91ce9fb0064ac7906b6e947ac
Jason Salerno <mrjsin@gmail.com>
2014-09-06 16:05:17 -0500

    Setting up proper route error checking

--- php/index.php
+++ php/index.php
@@ -4,11 +4,12 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('/:filepath/:functionName(/(:varName))', function ($filepath=null,$functionName=null,$varName=null) use ($app) {
+$app->get('(/(:filepath(/(:functionName(/(:varName))))))', function ($filepath=null,$functionName=null,$varName=null) use ($app) {
 	$home = new SimpleXMLElement("<Route/>");
-    $route = $home->addChild("filepath",$filepath);
-    $route = $home->addChild("functionName",$functionName);
-    $route = $home->addChild("varName",$varName);
+	$route = $home->addChild("filepath",$filepath);
+	$route = $home->addChild("functionName",$functionName);
+	$route = $home->addChild("varName",$varName);
+
 	echo $home->asXML();
 
 	//require __DIR__.'/' . $filepath . '.php';
