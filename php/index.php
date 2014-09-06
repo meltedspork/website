c86d218b6f7ee7d1215edc44e19674de58862a91
Jason Salerno <mrjsin@gmail.com>
2014-09-06 17:14:51 -0500

    debugging yet again...

--- php/index.php
+++ php/index.php
@@ -4,14 +4,14 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('(/(:filepath(/(:functionName(/(:varName(/(:var2Name))))))))', function ($filepath=null,$functionName=null,$varName=null,,$var2Name=null) use ($app) {
+$app->get('(/(:filepath(/(:functionName(/(:firstVar(/(:secondVar))))))))', function ($filepath=null,$functionName=null,$firstVar=null,,$secondVar=null) use ($app) {
 	//require __DIR__.'/' . $filepath . '.php';
 
 	$home = new SimpleXMLElement("<Route/>");
 	$route = $home->addChild("filepath",$filepath);
 	$route = $home->addChild("functionName",$functionName);
-	$route = $home->addChild("varName",$varName);
-	$route = $home->addChild("var2Name",$var2Name);
+	$route = $home->addChild("firstVar",$firstVar);
+	$route = $home->addChild("secondVar",$secondVar);
 
 	echo $home->asXML();
 
