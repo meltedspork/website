b89fcf21335b0f4dd1c069a8eb77f41e23840ae2
Jason Salerno <mrjsin@gmail.com>
2014-09-06 16:24:45 -0500

    Testing one more variable routing

--- php/index.php
+++ php/index.php
@@ -4,18 +4,17 @@ require str_replace('php', '', __DIR__ ) . 'vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
-$app->get('(/(:filepath(/(:functionName(/(:varName))))))', function ($filepath=null,$functionName=null,$varName=null) use ($app) {
-	require __DIR__.'/' . $filepath . '.php';
-	/*
+$app->get('(/(:filepath(/(:functionName(/(:varName(/(:var2Name))))))))', function ($filepath=null,$functionName=null,$varName=null,,$var2Name=null) use ($app) {
+	//require __DIR__.'/' . $filepath . '.php';
+
 	$home = new SimpleXMLElement("<Route/>");
 	$route = $home->addChild("filepath",$filepath);
 	$route = $home->addChild("functionName",$functionName);
 	$route = $home->addChild("varName",$varName);
+	$route = $home->addChild("var2Name",$var2Name);
 
 	echo $home->asXML();
-	*/
 
-	//require __DIR__.'/' . $filepath . '.php';
 });
 
 $app->run();
