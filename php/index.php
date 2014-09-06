492cc0df8039b1805e9c1ff7e0552e5ec8174e7a
Jason Salerno <mrjsin@gmail.com>
2014-09-06 16:06:38 -0500

    Now index.php are managing routing properly.

--- php/index.php
+++ php/index.php
@@ -5,12 +5,15 @@ $app = new \Slim\Slim();
 $app->response()->header("Content-Type", "application/xml");
 
 $app->get('(/(:filepath(/(:functionName(/(:varName))))))', function ($filepath=null,$functionName=null,$varName=null) use ($app) {
+	require __DIR__.'/' . $filepath . '.php';
+	/*
 	$home = new SimpleXMLElement("<Route/>");
 	$route = $home->addChild("filepath",$filepath);
 	$route = $home->addChild("functionName",$functionName);
 	$route = $home->addChild("varName",$varName);
 
 	echo $home->asXML();
+	*/
 
 	//require __DIR__.'/' . $filepath . '.php';
 });
